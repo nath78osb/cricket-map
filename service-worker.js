9e5956494b541e91573697bb80072110868da8fa
nath78osb <151663455+nath78osb@users.noreply.github.com>
2025-12-28 19:03:27 +0000

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'cricket-predictor-v1';
+const ASSETS_TO_CACHE = [
+    './',
+    './index.html',
+    './style.css',
+    './script.js',
+    './manifest.json',
+    './pwa_assets/icon-512.jpg'
+];
+
+// Install Event: Cache assets
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.addAll(ASSETS_TO_CACHE);
+            })
+    );
+});
+
+// Fetch Event: Serve from cache if available
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                return response || fetch(event.request);
+            })
+    );
+});
+
+// Activate Event: Cleanup old caches
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
